53fc43605d73cf7d7081ae12f160831c2c614dd3
Nthuyduong <nguyenthuyduong15113@gmail.com>
2023-11-26 22:17:50 +0700

    commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -13,33 +13,8 @@ import { useRouter } from 'next/router';
 import { ROUTES } from '../utils/constants';
 // import { handleRouteChange }  from '../utils/permission';
 
-// // NProgress Configuration
-// NProgress.configure({ showSpinner: false });
 
 const MyApp = ({ Component, pageProps }) => {
-
-    // const router = useRouter();
-    //
-    // useEffect(() => {
-    //
-    //     const handleRouteStart = () => NProgress.start();
-    //     const handleRouteDone = () => NProgress.done();
-    //
-    //     Router.events.on("routeChangeStart", handleRouteStart);
-    //     Router.events.on("routeChangeComplete", handleRouteDone);
-    //     Router.events.on("routeChangeError", handleRouteDone);
-    //
-    //     return () => {
-    //         // Make sure to remove the event handler on unmount!
-    //         Router.events.off("routeChangeStart", handleRouteStart);
-    //         Router.events.off("routeChangeComplete", handleRouteDone);
-    //         Router.events.off("routeChangeError", handleRouteDone);
-    //     };
-    // }, []);
-    //
-    // useEffect(() => {
-    //     handleRouteChange(router);
-    // }, [router.pathname])
 
     return(
         <Provider>
