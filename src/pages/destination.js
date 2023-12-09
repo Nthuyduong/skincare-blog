fdc88e02c074041843a27641fdc07aeaf291518b
Nthuyduong <nguyenthuyduong15113@gmail.com>
2023-12-09 17:33:59 +0700

    re-design category page

--- src/pages/destination.js
+++ src/pages/destination.js
@@ -2,7 +2,16 @@ const Destination = () => {
     return (
         <div>
             <div className="container-fluid">
-                <div className="heading">Destinations</div>
+                <div className="grid grid-cols-12 pt-5">
+                    <div className="col-span-7">
+                        <div className="heading">Guides & Tutorials</div>
+                    </div>
+                    <div className="col-span-5">
+                        Tempus, tristique morbi scelerisque sed. Diam nec ut sed est sit in tortor. Blandit
+                        consequat quisque vitae ornare diam netus tellus. Tempus, tristique morbi scelerisque sed
+                    </div>
+                </div>
+
                 {/*<div className="destination-banner relative">*/}
                     {/*<div className="banner-img">*/}
                     {/*    <img className="w-full" src="./img/destination/banner.jpg" alt="smile" loading="lazy"/>*/}
@@ -69,63 +78,39 @@ const Destination = () => {
             {/*all destination show here*/}
             <div className="container-fluid destination all-destination-cate">
                 <div className="grid grid-cols-12 gap-5">
-                    <div className="col-span-4 destination-cate mb-4">
+                    <div className="col-span-3 destination-cate mb-4">
                         <img className="w-full" src="./img/destination/des2.jpg" alt="smile" loading="lazy"/>
-                        <div className="text-center destination-cate-content">
-                            <div className="heading_6 my-1">Drink & Coffee</div>
-                            <div className="flex justify-center more-info">
-                                <div className="pr-2">398 Locations</div>
-                                <div className="pl-2">40 Articles</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-span-5 destination-cate">
-                        <img className="w-full" src="./img/destination/des5.jpg" alt="smile" loading="lazy"/>
-                        <div className="text-center destination-cate-content">
-                            <div className="heading_5 my-1">Drink & Coffee</div>
-                            <div className="flex justify-center more-info">
-                                <div className="pr-2">398 Locations</div>
-                                <div className="pl-2">40 Articles</div>
+                        <div className="destination-cate-content">
+                            <div className="heading_6 my-1">Help for beginner</div>
+                            <div className="flex more-info">
+                                <div className="pl-2 small_text">40 Articles</div>
                             </div>
                         </div>
                     </div>
                     <div className="col-span-3 destination-cate">
                         <img className="w-full" src="./img/destination/des3.jpg" alt="smile" loading="lazy"/>
-                        <div className="text-center destination-cate-content">
-                            <div className="heading_5 my-1">Drink & Coffee</div>
-                            <div className="flex justify-center more-info">
-                                <div className="pr-2">398 Locations</div>
-                                <div className="pl-2">40 Articles</div>
+                        <div className="destination-cate-content">
+                            <div className="heading_5 my-1">How to clean</div>
+                            <div className="flex more-info">
+                                <div className="pl-2 small_text">40 Articles</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-span-3 destination-cate">
+                        <img className="w-full" src="./img/destination/des5.jpg" alt="smile" loading="lazy"/>
+                        <div className="destination-cate-content">
+                            <div className="heading_5 my-1">Shopping guide</div>
+                            <div className="flex more-info">
+                                <div className="pl-2 small_text">40 Articles</div>
                             </div>
                         </div>
                     </div>
                     <div className="col-span-3 destination-cate">
                         <img className="w-full" src="./img/destination/des0.jpg" alt="smile" loading="lazy"/>
-                        <div className="text-center destination-cate-content">
-                            <div className="heading_5 my-1">Drink & Coffee</div>
-                            <div className="flex justify-center more-info">
-                                <div className="pr-2">398 Locations</div>
-                                <div className="pl-2">40 Articles</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-span-4 destination-cate">
-                        <img className="w-full" src="./img/destination/des4.jpg" alt="smile" loading="lazy"/>
-                        <div className="text-center destination-cate-content">
-                            <div className="heading_5 my-1">Drink & Coffee</div>
-                            <div className="flex justify-center more-info">
-                                <div className="pr-2">398 Locations</div>
-                                <div className="pl-2">40 Articles</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-span-5 destination-cate">
-                        <img className="w-full" src="./img/destination/des5.jpg" alt="smile" loading="lazy"/>
-                        <div className="text-center destination-cate-content">
-                            <div className="heading_5 my-1">Drink & Coffee</div>
-                            <div className="flex justify-center more-info">
-                                <div className="pr-2">398 Locations</div>
-                                <div className="pl-2">40 Articles</div>
+                        <div className="destination-cate-content">
+                            <div className="heading_5 my-1">Layering skincare</div>
+                            <div className="flex more-info">
+                                <div className="pl-2 small_text">40 Articles</div>
                             </div>
                         </div>
                     </div>
