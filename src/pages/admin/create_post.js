42f28d482be95e98c860e98fa68d9be98d505282
Nthuyduong <nguyenthuyduong15113@gmail.com>
2023-12-11 11:29:31 +0700

    create a new post page: creating setting bar

--- src/pages/admin/create_post.js
+++ src/pages/admin/create_post.js
@@ -1,22 +1,58 @@
-import React, { Component } from 'react';
+import React, {Component, useState} from 'react';
+import Test from "../test";
 
 const Createpost = () => {
 
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const openNav = () => {
+        setIsMenuOpen(true);
+    };
+
+    const closeNav = () => {
+        setIsMenuOpen(false);
+    };
+
     return(
-        <div>
-            <div>
-                <div>Create new post</div>
-                <div>Upload new blog article here!</div>
-            </div>
-            <div>
-                <div className="grid grid-cols-12 gap-5">
-                    <div className="col-span-9">
+        <div className="create-new-post">
+            <div className="create-post-top">
+                <div className="pt-5">
+                    <div className="heading_1">Create new post</div>
+                    <div>Upload new blog article here!</div>
+                </div>
+                <div className="grid grid-cols-12 gap-4 create-function pb-4 mb-4">
+                    <div className="col-span-5">
 
                     </div>
-                    <div className="col-span-3">
+                    <div className="col-span-2">
+                        <button className="w-full my-btn-pr w-full">Preview post</button>
+                    </div>
+                    <div className="col-span-2">
+                        <button className="w-full my-btn-pr w-full">Publish post</button>
+                    </div>
+                    <div className="col-span-2">
+                        <button className="w-full my-btn-pr w-full">Delete post</button>
+                    </div>
+                    <div className="col-span-1">
+                        <button onClick={openNav} className="">Setting</button>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div className="gap-4">
+                    <div className="create-content">
+                        <Test/>
+                    </div>
+                    <div className={`setting-bar ${
+                        isMenuOpen ? 'open' : ''
+                    }`}>
                         <div className="post-side-setting">
-                            <div className="">Blog settings</div>
-                            <div className=""></div>
+                            <div className="flex">
+                                <div className="heading_4 mr-auto">Blog settings</div>
+                                <button className="close" onClick={closeNav}>
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
                             <div className="my-3">
                                 <div className="mb-1">Blog URL blog</div>
                                 <div className="search-bar-box">
