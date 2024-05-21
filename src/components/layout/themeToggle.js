72c06a8f4e63dd705ff74ac7c010576146eec2c3
giangtx13 <giangtx13@gmail.com>
2024-05-21 20:32:42 +0700

    rm dark mode with device

--- src/components/layout/themeToggle.js
+++ src/components/layout/themeToggle.js
@@ -5,24 +5,25 @@ const themeToggle = () => {
     const themeToggleRef = useRef(null);
 
     useEffect(() => {
-        if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        // if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        if (localStorage.theme === 'dark') {
             document.documentElement.classList.add('dark');
             themeToggleRef.current.checked = true;
         } else {
             document.documentElement.classList.remove('dark');
             themeToggleRef.current.checked = false;
         }
-        window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
-            if (event.matches) {
-                document.documentElement.classList.add('dark');
-                localStorage.setItem('theme', 'dark');
-                themeToggleRef.current.checked = true;
-            } else {
-                document.documentElement.classList.remove('dark')
-                localStorage.removeItem('theme')
-                themeToggleRef.current.checked = false;
-            }
-        });
+        // window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+        //     if (event.matches) {
+        //         document.documentElement.classList.add('dark');
+        //         localStorage.setItem('theme', 'dark');
+        //         themeToggleRef.current.checked = true;
+        //     } else {
+        //         document.documentElement.classList.remove('dark')
+        //         localStorage.removeItem('theme')
+        //         themeToggleRef.current.checked = false;
+        //     }
+        // });
     }, []);
     const handleChangeTheme = (e) => {
         const theme = e.target.checked ? 'dark' : 'light';
