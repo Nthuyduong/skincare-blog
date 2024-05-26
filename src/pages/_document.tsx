e84b4bdc1e9a2d4486108341c06c4993970faf09
giangtx13 <giangtx13@gmail.com>
2024-05-26 23:08:28 +0700

    add analytic

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,16 +6,15 @@ export default function Document() {
     return (
         <Html lang="en">
             <link rel="icon" href="/favicon.png" />
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-3X4CD847DY"></script>
             <script
                 dangerouslySetInnerHTML={{
                 __html: `
-                    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-                    ga('create', 'UA-XXXXX-Y', 'auto');
-                    ga('send', 'pageview');
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                
+                    gtag('config', 'G-3X4CD847DY');
                 `,
                 }}
             />
