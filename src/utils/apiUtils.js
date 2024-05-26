831636383e2073eb6b2ca37426b3571bd9c18f30
giangtx13 <giangtx13@gmail.com>
2024-05-26 18:32:26 +0700

    chuyen nha

--- src/utils/apiUtils.js
+++ src/utils/apiUtils.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "https://app.radiance-aura.blog";
+export const BASE_URL = "https://api.radiance-aura.blog";
 // export const BASE_URL = "http://localhost:8000";
 
 export const fetchApi = axios.create({
